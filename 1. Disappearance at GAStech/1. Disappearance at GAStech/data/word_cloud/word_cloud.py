4431444338e18c7ae5ff77beb65a773f586202e4
Roy-C4 <s4aumyaroy@gmail.com>
2023-03-09 14:27:56 +0100

    Proj report

--- 1. Disappearance at GAStech/1. Disappearance at GAStech/data/word_cloud/word_cloud.py
+++ 1. Disappearance at GAStech/1. Disappearance at GAStech/data/word_cloud/word_cloud.py
@@ -33,17 +33,17 @@ with open('0.txt', 'r') as infile:
     text = infile.read()
 
 
-#cleaned_text = clean_text(text)
+cleaned_text = clean_text(text)
 
-#fig = cloud(cleaned_text)
+fig = cloud(cleaned_text)
 
-for file_name in os.listdir(in_dir):
+"""for file_name in os.listdir(in_dir):
     if file_name.endswith('.txt'):
         input_path  = os.path.join(in_dir, file_name)
         with open(input_path, 'r') as infile:
             text = infile.read()
             cleaned_text = clean_text(text)
-            fig = cloud(cleaned_text)
+            fig = cloud(cleaned_text)"""
 
 
 app = dash.Dash()
