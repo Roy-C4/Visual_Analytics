665f56119b1b54975109359c66337b07f65b7fa7
Roy-C4 <s4aumyaroy@gmail.com>
2023-03-30 17:52:19 +0200

    Resume filter

--- Disappearance at GAStech/data/Background/Resume_word_count.py
+++ Disappearance at GAStech/data/Background/Resume_word_count.py
@@ -26,22 +26,23 @@ for file_name in os.listdir(in_dir_path):
         with open(input_path, 'r') as infile:
             text = infile.read()    
             tokenized_corpus = [nltk.word_tokenize(doc.lower()) for doc in text]
-            stop_words = set(nltk.corpus.stopwords.words('english'))
-            filtered_corpus = [[word for word in doc if word not in stop_words] for doc in tokenized_corpus]
-
-            cv = CountVectorizer()
-            X = cv.fit_transform([' '.join(doc) for doc in filtered_corpus])
-            words = cv.get_feature_names()
-            word_counts = cv.transform([text])
+            print(tokenized_corpus)
+            cleaned_text = clean_text(tokenized_corpus)
+            # filtered_corpus = [[word for word in doc if word not in stop_words] for doc in tokenized_corpus]
+            # print(filtered_corpus)
+#             cv = CountVectorizer()
+#             X = cv.fit_transform([' '.join(doc) for doc in filtered_corpus])
+#             words = cv.get_feature_names()
+#             word_counts = word_counts.toarray()
             
-            for i in range(len(words)):
-                word_dict[words[i]] = word_counts.toarray()[0][i]
+#             for i in range(len(words)):
+#                 word_dict[words[i]] = word_counts[0][i]
 
-sorted_dict = {k: v for k, v in sorted(word_dict.items(), key=lambda item: item[1], reverse=True)}
+# sorted_dict = {k: v for k, v in sorted(word_dict.items(), key=lambda item: item[1], reverse=True)}
 
 
 
-print(sorted_dict)
+# print(sorted_dict)
                 
 
             
--- Disappearance at GAStech/data/Background/resume.py
+++ Disappearance at GAStech/data/Background/resume.py
@@ -5,10 +5,10 @@ from contextlib import redirect_stdout
 from rake_nltk import Rake
 rake_nltk_var = Rake()
 
-in_dir_path = r'D:/roy/selected/TU/books/visual_analytics/Disappearance at GAStech/data/resumes/txt versions'
-out_dir_path = r'D:/roy/selected/TU/books/visual_analytics/Disappearance at GAStech/data/output_resume'
+in_dir_path = r'D:\roy\selected\TU\books\visual_analytics\Disappearance at GAStech\data\Background\resumes\txt_versions'
+out_dir_path = r'D:\roy\selected\TU\books\visual_analytics\Disappearance at GAStech\data\Background\output_resume'
 
-keywords = [" Armed forces of Kronos", "Protectors of Kronos", "Kronos Armed"]
+keywords = [" Armed forces of Kronos", "Protectors of Kronos", "Kronos Armed", "Armed", "Forces", "Protectors", "PoK"]
 
 for file_name in os.listdir(in_dir_path):
     if file_name.endswith('.txt'):
@@ -17,10 +17,21 @@ for file_name in os.listdir(in_dir_path):
         with open(input_path, 'r') as infile:
             with open(output_path, 'w') as outfile:
                 text = infile.read()
-                print(type(text))
-                for k in keywords: 
-                    if k.lower() in text.lower():
+                for j in text.split():
+                    if j in keywords:
+                        print("keyword present", j)
                         outfile.write("%s\n"%text)
                         print("File moved succesfully")
                     else:
                         print("Unsuccesfull")
+
+for filename in os.listdir(out_dir_path):
+    file_path = os.path.join(out_dir_path, filename)
+    
+    # check if the file is a text file and is  not a directory
+    if filename.endswith('.txt') and os.path.isfile(file_path):
+        
+        # check if the file is empty
+        if os.stat(file_path).st_size == 0:
+            os.remove(file_path) # delete the file
+            print(f"{filename} has been deleted")
