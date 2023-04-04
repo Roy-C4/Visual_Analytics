66c5d267da48f9a3166d009f54c1622bdf7d5e65
Dido Verstegen <didoverstegen123@gmail.com>
2023-04-04 14:25:08 +0200

    merge addition

--- Disappearance at GAStech/data/word_cloud/data_prep.py
+++ Disappearance at GAStech/data/word_cloud/data_prep.py
@@ -55,3 +55,90 @@ in_dir = "D:/roy/selected/TU/books/visual_analytics/Disappearance at GAStech/dat
 alltext = all_text(in_dir)
 #print(alltext['844.txt'])
 #print(len(alltext.keys()))
+
+def plot_timeline(articles):
+    df = pd.DataFrame.from_dict(articles, 'index', columns=['Date', 'Keywords']).reset_index()
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Article length'] = df['Keywords'].apply(lambda x: len(x.split(' ')))
+    
+    fig = px.scatter(df, x='Date', y='Article length', title='Timeline with rangeslider')
+    fig.update_xaxes(rangeslider_visible=True)
+    fig.show()
+
+def find_keywords(all_text):
+    pass
+
+
+def create_frequency_dataframe():
+    in_dir = "C:/Users/didov/Desktop/DS&AI/Q3_VisualAnalytics/Visual_Analytics/Disappearance at GAStech/data/articles"
+    alltext = all_text(in_dir)
+    nodate={}
+    dates={}
+    for key, value in alltext.items():
+    #     for i in value[1].split():
+    #         if i == 'aardgasbedrijf':
+    #             print(key)
+        nodate[key] = value[1]
+        dates[key] = value[0]
+
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(list(nodate.values()))
+    word_list = vectorizer.get_feature_names_out()
+
+    df = pd.DataFrame(X.todense(), index=nodate.keys(), columns=vectorizer.get_feature_names_out())
+    df['Date'] = dates
+    df = df.sort_values(by='Date')
+
+    return df
+
+# in_dir = "C:/Users/didov/Desktop/DS&AI/Q3_VisualAnalytics/Visual_Analytics/Disappearance at GAStech/data/articles"
+# alltext = all_text(in_dir)
+# sentence = alltext['0.txt'][1]
+# # lemmatizer = WordNetLemmatizer()
+# # out = " ".join([lemmatizer.lemmatize(wd) for wd in sentence.split()])
+# nodate={}
+# for key, value in alltext.items():
+#     for i in value[1].split():
+#         if i == 'aardgasbedrijf':
+#             print(key)
+#     nodate[key] = value[1]
+
+# vectorizer = CountVectorizer()
+# X = vectorizer.fit_transform(nodate.values())
+# word_list = vectorizer.get_feature_names_out()
+
+# # Added [0] here to get a 1d-array for iteration by the zip function. 
+# count_list = np.asarray(X.sum(axis=0))[0]
+# word_counts = dict(zip(word_list, count_list))
+# l = {k: v for k, v in sorted(word_counts.items(), key=lambda item: item[1], reversed=True)}
+
+# df = pd.DataFrame(X.todense(), index=nodate.keys(), columns=vectorizer.get_feature_names_out())
+
+
+def get_keywords_dataframe(dataframe, keywords):
+#     keyword = keywords.lower()
+
+    keywords.append('Date')
+    df = dataframe[keywords]
+    df_long = df.melt(id_vars='Date', var_name='Word', value_name='Frequency')
+    
+    return df_long
+
+def plot_timeline(dataframe, keywords):
+#     keyword = keywords.lower()
+    keywords.append('Date')
+    df = dataframe[keywords]
+    df_long = df.melt(id_vars='Date', var_name='Word', value_name='Frequency')
+    fig = px.bar(df_long, x='Date', y='Frequency', title='Occurance of keywords', color='Word')
+    fig.update_xaxes(rangeslider_visible=True)
+    fig.show()
+    
+# plot_timeline(df, ['death', 'fire', 'pok'])
+# # print(l) 
+# # print(word_counts['death'])
+# # print(sorted(word_counts))
+# # print(X)
+# # print(alltext)
+# plot_timeline(alltext)
+# print(alltext['844.txt'])
+# print(len(alltext.keys()))
